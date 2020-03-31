d4dcba878eaf0908d0e43aa7ab1f126e57f62313
DineshChirnanchu <Dinesh516ch@outlook.com>
2020-03-31 20:02:15 +0530

    updated coded

--- CodedUITestProject3/CodedUITestProject3/Properties/AssemblyInfo.cs
+++ CodedUITestProject3/CodedUITestProject3/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CUITProject")]
+[assembly: AssemblyTitle("CodedUITestProject3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CUITProject")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("CodedUITestProject3")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d1bd0272-979f-4ab7-87f1-db1d763cd3e6")]
+[assembly: Guid("75bc1a34-3746-414c-8123-c0c0e89032c3")]
 
 // Version information for an assembly consists of the following four values:
 //
